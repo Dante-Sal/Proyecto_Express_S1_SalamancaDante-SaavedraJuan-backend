bdf6044fde8f92142959013dc4ca52dcb6e196b1
Dante-Sal <dantecampuslands@gmail.com>
2025-09-25 22:15:46 -0500

    fix: :ambulance: Refining CORS policy

--- app.js
+++ app.js
@@ -10,8 +10,21 @@ const app = express();
 let PORT = parseInt(process.env.PORT?.trim());
 if (!Number.isFinite(PORT) || PORT < 3000 || PORT > 3999) PORT = 3103;
 
+const allowed = ['https://dante-sal.github.io', 'http://127.0.0.1:5500', 'http://localhost:5500']
+
+const options = {
+    origin(origin, callback) {
+        if (!origin || allowed.includes(origin)) return callback(null, true);
+        return callback(new Error('CORS error (origin not allowed)'));
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(options));
+app.options('*', cors(options));
 app.use(express.json());
-app.use(cors({ origin: 'https://dante-sal.github.io' }));
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs));
 app.use('/', require('./routes'));
 
